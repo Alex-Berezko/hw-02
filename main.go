7b49825aaf195eb90ebaefa4469e8dca4d24b5d3
Alex Berezko Mac <berezko33@gmail.com>
2026-06-22 18:44:29 +0300

    HW 3

--- main.go
+++ main.go
@@ -3,6 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
-	fmt.Println("hello git")
+	var a int
+	fmt.Println("Введите количество ячеек а")
+	fmt.Scan(&a)
+
+	for i := 0; i < a; i++ {
+		if i%2 == 0 {
+			for j := 0; j < a; j++ {
+				if j%2 == 0 {
+					fmt.Print("#")
+				} else {
+					fmt.Print(" ")
+				}
+
+			}
+			fmt.Print("\n")
+		} else {
+			for j := 0; j < a; j++ {
+
+				if j%2 != 0 {
+					fmt.Print("#")
+				} else {
+					fmt.Print(" ")
+				}
+
+			}
+			fmt.Print("\n")
+		}
+	}
 }
